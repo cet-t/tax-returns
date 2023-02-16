d838f4a18ec93595c711f1352d3ba46ef57627a8
cet <cets021@gmail.com>
2023-02-16 22:18:04 +0900

    update: wip

--- taxreturn.py
+++ taxreturn.py
@@ -1,32 +1,41 @@
-class defs:
-    def auto_sum(_sum) -> int:
-        ret: int = sum(_sum)
-        return ret
+sheets = [
+    'hiroko_med',
+    'hiroko_nur',
+    'takashi_med',
+    'takashi_nur'
+]
 
-    def mn(_med, _nur, _name) -> None:
-        hm = defs.auto_sum(_med)
-        hn = defs.auto_sum(_nur)
-        print(f'{_name}: {hm}')
-        print(f'{_name}: {hn}')
-        print(f'sum: {hm+hn}')
+max_row = {
+    'hm': 30,
+    'hn': 39,
+    'tm': 64,
+    'tn': 75
+}
+
+h = 'hiroko'
+t = 'takashi'
+
+path = './tax_return2.xlsx'
+
+
+def tax_culc(_name: str, _sheet: int, _max: int):
+    file = openpyxl.load_workbook(path)
+    sheet = file[_sheet]
+    sums = []
+    c: int
+    for i in range(1, _max, 1):
+        datas = sheet.cell(row=i, column=2).value
+        sums.append(datas)
+        c += i
+    file.close()
+
+    print(f'{_name}: {sum(sums)}({c})')
 
 
 if __name__ == "__main__":
     import openpyxl
-    import datas
 
-    # defs.mn(datas.hiro_med, datas.hiro_nur, "hiroko")
-    # defs.mn(datas.taka_med, datas.taka_nur, "takashi")
-
-    sheets = [
-        'hiroko_med',
-        'hiroko_nur',
-        'takashi_med',
-        'takashi_nur'
-    ]
-
-    file = openpyxl.load_workbook('./tax_return2.xlsx')
-    sheet = file[sheets[0]]
-    v = sheet.cell(row=1, column=1).value
-    print(v)
-    file.close()
+    tax_culc(h, sheets[0], max_row['hm'])
+    tax_culc(h, sheets[1], max_row['hn'])
+    tax_culc(t, sheets[2], max_row['tm'])
+    tax_culc(t, sheets[3], max_row['tn'])
