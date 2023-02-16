15cb817d556a819045c08dfd9879b10d1816a029
cet <67916285+cet-t@users.noreply.github.com>
2023-02-17 02:53:40 +0900

    update: remove unrequited codes

--- src/taxreturn.py
+++ src/taxreturn.py
@@ -21,13 +21,11 @@ names = [
 
 path = 'data/tax_return2.xlsx'
 
-datas: int
-
-
 def tax_culc(_name: str, _sheet: int, _max: int):
     file = load_workbook(path)
     sheet = file[_sheet]
     sums = []
+    datas: int
     for i in range(1, _max, 1):
         datas = sheet.cell(i, 2).value
         sums.append(datas)
@@ -36,23 +34,8 @@ def tax_culc(_name: str, _sheet: int, _max: int):
     file.close()
 
 
-def all_tax(_sheet: int, _max: int) -> int:
-    file = load_workbook(path)
-    sheet = file[_sheet]
-    adds: int
-    for i in range(1, _max, 1):
-        dts = sheet.cell(i, 2)
-        adds += dts
-    file.close()
-    return adds
-
-
 if __name__ == '__main__':
     from openpyxl import *
 
-    taxes: list[int] = []
     for i in range(4):
         tax_culc(names[i], sheets[i], max_row[i])
-
-    for i in range(4):
-        print(all_tax(sheets[i], max_row[i]))
