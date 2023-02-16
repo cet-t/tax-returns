78dbea43f5f66d2f383f7cbec54b86f0ad5a77a3
cet <cets021@gmail.com>
2023-02-17 02:16:21 +0900

    numa

--- src/taxreturn.py
+++ src/taxreturn.py
@@ -29,15 +29,22 @@ def tax_culc(_name: str, _sheet: int, _max: int):
     sheet = file[_sheet]
     sums = []
     for i in range(1, _max, 1):
-        datas = sheet.cell(row=i, column=2).value
+        datas = sheet.cell(i, 2).value
         sums.append(datas)
     print(f'{_name}: {sum(sums)}')
 
     file.close()
 
 
-def all_tax():
-    pass
+def all_tax(_sheet: int, _max: int) -> int:
+    file = load_workbook(path)
+    sheet = file[_sheet]
+    adds: int
+    for i in range(1, _max, 1):
+        dts = sheet.cell(i, 2)
+        adds += dts
+    file.close()
+    return adds
 
 
 if __name__ == '__main__':
@@ -46,3 +53,6 @@ if __name__ == '__main__':
     taxes: list[int] = []
     for i in range(4):
         tax_culc(names[i], sheets[i], max_row[i])
+
+    for i in range(4):
+        print(all_tax(sheets[i], max_row[i]))
