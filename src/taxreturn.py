6ed8dbfdb7bb0d53036e4fb0ed80817e761ea174
cet <cets021@gmail.com>
2023-02-17 22:24:21 +0900

    not fix: not yet

--- src/taxreturn.py
+++ src/taxreturn.py
@@ -20,11 +20,9 @@ titles = [
     'nursing',
 ]
 
-path = 'data/tax_return2.xlsx'
-
 
 def tax_culc(_name: str, _sheet: int, _max: int) -> None:
-    file = load_workbook(path)
+    file = openpyxl.load_workbook(path)
     sheet = file[_sheet]
     sums = []
     for i in range(1, _max, 1):
@@ -36,30 +34,39 @@ def tax_culc(_name: str, _sheet: int, _max: int) -> None:
 
 
 def tax_sum(_sheet: int, _max: int) -> int:
-    file = load_workbook(path)
+    file = openpyxl.load_workbook(path)
     sheet = file[_sheet]
     sums = []
     for i in range(1, _max, 1):
         datas = sheet.cell(i, 2).value
         sums.append(datas)
+    file.close()
     return sum(sums)
 
-    file.close()
 
+sums: list[int]
+sums = [
+    tax_sum(sheets[0], max_row[0]),
+    tax_sum(sheets[1], max_row[1]),
+    tax_sum(sheets[2], max_row[2]),
+    tax_sum(sheets[3], max_row[3]),
+]
+all_: int = 0
+alls = [
+    sums[0],  # 医1
+    sums[1],  # 介1
+    sums[2],  # 医2
+    sums[3],  # 介2
+    sums[0]+sums[2],  # 医合計
+    sums[1]+sums[3],  # 介合計
+    sums[0]+sums[1],  # 合計1
+    sums[2]+sums[3],  # 合計2
+    sum(sums),
+]
 
 if __name__ == '__main__':
-    from openpyxl import *
+    import openpyxl
 
-    sums: list[int]
-    sums = [
-        tax_sum(sheets[0], max_row[0]),
-        tax_sum(sheets[1], max_row[1]),
-        tax_sum(sheets[2], max_row[2]),
-        tax_sum(sheets[3], max_row[3]),
-    ]
-    alls = [
-        sums[0],
-    ]
-
-    for i in range(len(sums)):
-        print(sums[i])
+    path = 'data/tax_return2.xlsx'
+    # for i in range(len(sums)):
+    # print(sums[i])
