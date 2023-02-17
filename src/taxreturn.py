74169e821f98b756029cacb1f4b6f18e7d29e0a7
cet <cets021@gmail.com>
2023-02-17 18:40:17 +0900

    update: sepalate result

--- src/taxreturn.py
+++ src/taxreturn.py
@@ -24,7 +24,7 @@ path = 'data/tax_return2.xlsx'
 datas: int
 
 
-def tax_culc(_name: str, _sheet: int, _max: int):
+def tax_culc(_name: str, _sheet: int, _max: int) -> None:
     file = load_workbook(path)
     sheet = file[_sheet]
     sums = []
@@ -36,9 +36,26 @@ def tax_culc(_name: str, _sheet: int, _max: int):
     file.close()
 
 
+def tax_sum(_sheet: int, _max: int) -> int:
+    file = load_workbook(path)
+    sheet = file[_sheet]
+    sums = []
+    for i in range(1, _max, 1):
+        datas = sheet.cell(i, 2).value
+        sums.append(datas)
+    return sum(sums)
+
+    file.close()
+
+
 if __name__ == '__main__':
     from openpyxl import *
 
     taxes: list[int] = []
     for i in range(4):
         tax_culc(names[i], sheets[i], max_row[i])
+    sums: dict[str, int]
+    sums = tax_sum(sheets[0], max_row[0])
+    sums = tax_sum(sheets[1], max_row[1])
+    sums = tax_sum(sheets[2], max_row[2])
+    sums = tax_sum(sheets[3], max_row[3])
