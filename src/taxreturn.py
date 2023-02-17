2ec6932143daa00145e5e0ad0e58408f59d271a7
cet <cets021@gmail.com>
2023-02-17 18:27:13 +0900

    new

--- src/taxreturn.py
+++ src/taxreturn.py
@@ -36,23 +36,9 @@ def tax_culc(_name: str, _sheet: int, _max: int):
     file.close()
 
 
-def all_tax(_sheet: int, _max: int) -> int:
-    file = load_workbook(path)
-    sheet = file[_sheet]
-    adds: int
-    for i in range(1, _max, 1):
-        dts = sheet.cell(i, 2)
-        adds += dts
-    file.close()
-    return adds
-
-
 if __name__ == '__main__':
     from openpyxl import *
 
     taxes: list[int] = []
     for i in range(4):
         tax_culc(names[i], sheets[i], max_row[i])
-
-    for i in range(4):
-        print(all_tax(sheets[i], max_row[i]))
